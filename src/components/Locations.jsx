094ea39d381efe42c0ccbafe39646eeace49052c
KevinFigueroasa <kevin.figueroasa@gmail.com>
2022-10-12 17:30:21 -0500

    Add Filter Search

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -4,32 +4,93 @@ import ResidentInfo from './ResidentInfo'
 
 const Locations = () => {
 
-    const [locationId, setLocationId] = useState("")
+    const [inputValue, setInputValue] = useState("")
+    const [suggestions, setSuggestions] = useState([])
     const [location, setLocation] = useState({})
+    const [selectedLocation, setSelectedLocation] = useState('')
 
     useEffect(() => {
         const randomId = Math.floor(Math.random() * 126) + 1
         axios
-        .get(`https://rickandmortyapi.com/api/location/${randomId}`)
-        .then(rest => setLocation(rest.data))
+            .get(`https://rickandmortyapi.com/api/location/${randomId}`)
+            .then(rest => setLocation(rest.data))
     }, [])
 
     const searchLocationById = () => {
         axios
-        .get(`https://rickandmortyapi.com/api/location/${locationId}`)
+            .get(`https://rickandmortyapi.com/api/location/${inputValue}`)
+            .then(res => setLocation(res.data))
+    }
+
+    // console.log('suggestions', suggestions)
+
+    const onShowSuggest = ({ target }) => {
+        setInputValue(target.value);
+
+        if (target.value) {
+            document.querySelector('.search').classList.add('open'); // Esto hace que la Opacidad establecida en 0 en la clase Suggests se convierta en 1 al agregar la clase open que tienen la propiedad opacity: 1;
+        } else {
+            document.querySelector('.search').classList.remove('open');
+        }
+
+    }
+
+    useEffect(() => {
+        axios
+            .get(`https://rickandmortyapi.com/api/location?name=${inputValue}`)
+            .then(res => setSuggestions(res.data.results))
+    }, [inputValue])
+
+    useEffect(() => {
+        document.querySelector('.search').classList.remove('open');
+        setInputValue('');
+    }, [selectedLocation])
+
+    const onSelectedLocation = ({ target }) => {
+        setSelectedLocation(target.textContent)
+        axios
+        .get(`https://rickandmortyapi.com/api/location/${suggestions[0].id}`)
         .then(res => setLocation(res.data))
+        setInputValue('');
     }
 
     return (
         <div className='header_app'>
             <div className='header_input'>
-                <div className='search'>
-                <input type="text" 
-                value={locationId}
-                onChange={e => setLocationId(e.target.value)}
-                placeholder='Search a location'/>
-                <button onClick={searchLocationById}>Run</button>
-                </div>
+                <form
+                    className='search center-item'
+                    onSubmit={e => e.preventDefault()}
+                >
+                    <input
+                        className='search__input'
+                        type="text"
+                        value={inputValue}
+                        onChange={onShowSuggest}
+                        placeholder='Search a location'
+                    />
+
+                    <ul className="suggests">
+                        {
+                            suggestions?.length
+                                ?
+                                <>
+                                    {
+                                        suggestions.map(suggestion => (
+                                            <li
+                                                onClick={onSelectedLocation}
+                                                className="suggest__item"
+                                                key={suggestion.id}>{suggestion.name}</li>
+                                        ))
+                                    }
+                                </>
+                                :
+                                <li>No results</li>
+                        }
+                    </ul>
+
+                    <button onClick={searchLocationById}>Run</button>
+                </form>
+
             </div>
 
             <div className='resident-info'>
@@ -47,19 +108,19 @@ const Locations = () => {
                         <p>{location.residents?.length}</p>
                     </h3>
                 </div>
-    
+
                 <div className='characters'>
                     <h1>{location.name}</h1>
                     <div className='residents'>
                         {
                             location.residents?.map((resident) => (
-                                <ResidentInfo key={resident} resident={resident}/>
+                                <ResidentInfo key={resident} resident={resident} />
                             ))
                         }
                     </div>
                 </div>
-                </div>
             </div>
+        </div>
     );
 };
 
